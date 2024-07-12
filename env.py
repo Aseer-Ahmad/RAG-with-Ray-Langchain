57dbd3571d6e9d1a0f788d2f08f45bd1be72d6a4
Aseer-Ahmad <a3ahmad.kiit@gmail.com>
2024-07-12 16:33:17 +0200

    vertex test

--- env.py
+++ env.py
@@ -1,5 +1,7 @@
 import os
 import ray
+import vertexai
+
 
 def setup_env():
     print("setting up environment")
@@ -10,6 +12,10 @@ def setup_env():
 
     os.environ["RAY_memory_usage_threshold"] = "1"
     os.environ['CPU_OR_GPU'] = 'cpu'
+    os.environ["GOOGLE_API_KEY"] = open("/home/cepheus/Documents/API keys/googleai.txt", 'r').read()
+    # vertexai.init(project=PROJECT_ID)
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/cepheus/Documents/API keys/gap_cred.json"
+
     # Setting up our Ray environment
     ray.init(runtime_env={
         "env_vars": {
--- main.py
+++ main.py
@@ -2,15 +2,32 @@ from preprocess.ray_data import ray_dataset
 from rag.indexing import indexer
 from env import setup_env
 import ray
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 def main():
     working_dir = "downloaded_docs"
     start_url = "https://python.langchain.com/docs/expression_language/"
 
     setup_env()
+
+    # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-004")
+    # vector = embeddings.embed_query("hello, world!")
+    # print(vector[:5])
+    
+    # embeddings = VertexAIEmbeddings()
+    # query_result = embeddings.embed_query("This is a test document.")
+    # print(query_result)
+    
     content_ds = ray_dataset(start_url, working_dir)
     print(ray.available_resources())
-    indexer(content_ds)
+    
+
+
+
+    # indexer(content_ds)
     # ray.get(indexer.options(num_cpus=.1, num_gpus=0).remote(content_ds))
 
 if __name__ == "__main__":
